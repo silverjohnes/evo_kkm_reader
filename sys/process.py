424cc3dc53d80a8c54fb2b9e1b3d82fd13fc48f9
silverjohnes <silver.johnes@gmail.com>
2021-10-23 00:09:44 +0300

    Add files via upload

---
+++ sys/process.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+# Файл обработки строк
+import os
+import sys
+import sqlite3
+from dict import dict
+
+# Подключение БД
+hddDb = sqlite3.connect(os.path.join(os.path.dirname(__file__), 'db.db'))
+db = sqlite3.connect(':memory:')
+hddDb.backup(db)
+hddDb.close()
+cursor = db.cursor()
+
+
+#
+# Обработчик строк:
+#
+def process(line, command):
+	if len(line) > 14:  # проверка для обработки пустых строк
+	
+		# ОБРАБОТЧИКИ КОМАНД ТУТ:
+		# 1. Вход в режим
+		if line[14:18] == '> 56':
+			line = line.rstrip() + " Вход в режим"
+			var = line[19:21],
+			cursor.execute("SELECT DESC FROM REGIME WHERE HEX = ?", var)
+			line = line.strip() + cursor.fetchone()[0] + "\n"
+				
+
+		# 2. Ответ <55h> <Код ошибки (1)>
+		if line[14:18] == '< 55' and len(line) == 26:
+			#if command in ("""XX, XX"""):  # Перечень команд, к которым может относиться этот ответ
+			if command not in ("""45"""): # Перечень, к которым ответ не применим
+				line = line.rstrip() + " Ответ:"
+				var = line[19:21],
+				cursor.execute("SELECT DESC FROM ERROR WHERE HEX = ?", var)
+				line = line.rstrip() + " " + str.lower(cursor.fetchone()[0]) + "\n"
+			elif command == "45": # Конкретно к какой комманде применим
+				line = line.rstrip() + " В режиме:"
+				var = line[19:21],
+				cursor.execute("SELECT DESC FROM STATUS_CODE WHERE BIN = ?", var)
+				line = line.rstrip() + " " + str.lower(cursor.fetchone()[0]) + "\n"
+	
+	
+	
+		#
+		# ЗАКОНЧИЛИСЬ ОБРАБОТЧИКИ СТРОК
+		#	
+		
+	return line
+#
+# FIN
+#
--- sys/test.py
+++ sys/test.py
@@ -1,8 +1,19 @@
 import os, sys
 import sqlite3
 
+print(len("12:22:21.024 << 55 00 00 "))
 
-db = sqlite3.connect(os.path.join(os.path.dirname(__file__), 'protocol.db'))
+
+
+
+
+
+
+
+'''
+Подключение к БД
+
+db = sqlite3.connect(os.path.join(os.path.dirname(__file__), 'db.db'))
 cursor = db.cursor()
 var = '00'
 cursor.execute("SELECT DESC FROM ERRORS WHERE HEX = '%s'" % var)
@@ -12,3 +23,11 @@ print(result)
 
 db.close()
 #os.system("pause")
+'''
+
+
+'''
+sys.byteorder - порядок байтов. Будет иметь значение 'big' при порядке следования битов от старшего к младшему, и 'little', если наоборот (младший байт первый).
+
+
+'''
