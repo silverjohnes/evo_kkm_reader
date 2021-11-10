61e5f59e4e26517db6996fb2c86d21a724315b44
silverjohnes <silver.johnes@gmail.com>
2021-11-10 22:39:30 +0300

    Исправлена работа с аргументами запуска

--- evo_kkm_reader.py
+++ evo_kkm_reader.py
@@ -12,27 +12,13 @@ sys.path.append(os.path.join(os.path.dirname(__file__), 'sys'))
 import process
 
 
-inTest = 0
+inTest = 1
 testInputFile = 'logs.txt'
 outputPrefix = 'processed_'
 
 
-# Определение рабочей папки для i-того количества входящих файлов.  
-if len(sys.argv) > 1: 
-	os.chdir(os.path.dirname(sys.argv[1]))
-elif inTest == 0:
-	print("Перетяните файлы с логами из любой папки на", os.path.basename(__file__), "\n")
-	os.system("pause")
-	quit()
-
-
-print("Сформированы файлы:")
-for i in range(len(sys.argv)):
-	if len(sys.argv) > 1: # Проверка на drag & drop.  
-		inputFile = os.path.basename(sys.argv[i+1])
-	elif inTest == 1: # Переменные для тестового режима:  
-		os.chdir(os.path.dirname(__file__))
-		inputFile = testInputFile
+# Обработчик файлов.
+def fileProcess():
 	outputFile = outputPrefix + inputFile
 	command = "00" # Управляющая команда, на которую обрабатывается ответ.  
 	with open(inputFile, 'r', encoding='utf-8') as input, open(outputFile, 'w', encoding='utf-8') as output:
@@ -40,8 +26,31 @@ for i in range(len(sys.argv)):
 			if line[14:16] == "> ":
 				command = line[16:18]
 			output.write(process.process(line, command)) # То, ради чего всё затевалось.
-			
+	print(">", outputFile)
 
-	print("", outputFile)
-print("Работа завершена.\n")
-#os.system("pause")
+
+# Определение рабочей папки или отбивка.
+if len(sys.argv) > 1: 
+	os.chdir(os.path.dirname(sys.argv[1]))
+elif inTest == 0:
+	print("Перетяните файлы с логами из любой папки на", os.path.basename(__file__), "\n")
+	if sys.platform == 'win32':
+		os.system('pause')
+	else:
+		input("Для продолжения нажмите Enter . . .")
+	quit()
+
+
+# Беги, глупец
+print("Сформированы файлы:")
+if len(sys.argv) > 1: # Есть параметры запуска?
+	for i in range(1, len(sys.argv)):
+		inputFile = os.path.basename(sys.argv[i])
+		fileProcess()
+elif inTest == 1: # Фигачу тестовый файл если включен тестовый режим
+	os.chdir(os.path.dirname(__file__))
+	inputFile = testInputFile
+	fileProcess()
+	
+	
+print()
