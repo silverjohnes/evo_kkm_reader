39a675c7a1b357fe0b61796ff551913ab57d5f01
silverjohnes <silver.johnes@gmail.com>
2022-02-02 11:01:12 +0300

    Расшифровка в E8, E9

--- sys/process.py
+++ sys/process.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Файл обработки строк
+# Файл обработки строк.
 import os
 import sys
 import sqlite3
@@ -9,18 +9,19 @@ errorCommand = "+=== Команда не известна или ошибка о
 errorSubCommand = "=+== Подкомманда не известна или ошибка обработки строки. Проверьте обновление программы"
 errorTag = "(описание отсутствует, проверьте обновление программы) ==+="
 
+bitMaskError = "Не удалось определить: передайте автору программы, что нужно исправить чтение бинарной маски ===+"
 
-# Подключение БД
+
+# Подключение БД.
 db = sqlite3.connect(':memory:')
 reaDb = sqlite3.connect(os.path.join(os.path.dirname(__file__), 'db.db'))
 reaDb.backup(db)
 reaDb.close()
 cursor = db.cursor()
 
-#
-# Обработчик строк:
-#
-def process(line, command, subCommand):
+
+# Обработка строк.
+def process(line, command, wholeCommandLine):
 	if len(line) > 14:  # проверка для обработки пустых строк
 	
 		# 
@@ -109,7 +110,7 @@ def process(line, command, subCommand):
 					except:
 						line = " ".join([line.rstrip(), "Демонстрационная печать:", errorSubCommand, "\n"])
 				
-				# Расшифровка легенды запроса из Протокола. Не соответствует Эвотору и загрязняет результат визуально.
+				# Расшифровка легенды запроса из Протокола. Не соответствует Эвотору по длинам аргументов и загрязняет результат визуально.
 				# try:
 					# hex_2 = line[19:21],
 					# cursor.execute("SELECT DESC FROM REGISTER WHERE HEX_2 = ?", hex_2)
@@ -119,18 +120,22 @@ def process(line, command, subCommand):
 				
 				
 			elif command == 'E8':
-				valueBlockNumber = int(line[25:27], 16)
-				if valueBlockNumber == 0:			
+				global valueBlockNumberW
+				valueBlockNumberW = int(line[25:27], 16)
+				if valueBlockNumberW == 0:
+					printString = line[40:].rstrip()
+					decode(printString, "outer")
 					try:	
 						hex_btswpt = line[28:33],
 						cursor.execute("SELECT TAG, NAME FROM TAGS WHERE HEX_BYTESWAPPED = ?", hex_btswpt)
 						tagAndName = cursor.fetchone()
-						line = "".join([line.rstrip(), " Запись реквизита ", tagAndName[0], " \"", tagAndName[1], "\"", " \n"])
+						line = "".join([line.rstrip(), " Запись реквизита ", tagAndName[0], " \"", tagAndName[1], "\": \"", decodedLine, "\" \n"])
 					except:
 						tagSwypedBack = str(int("".join([line[31:33], line[28:30]]), 16))
 						line = " ".join([line.rstrip(), "Запись реквизита", tagSwypedBack, errorTag, "\n"])
 				else:
-					line = " ".join([line.rstrip(), "продолжение записи реквизита, блок", str(valueBlockNumber + 1), "\n"])
+					printString = line[34:].rstrip()
+					line = " ".join([line.rstrip(), "продолжение записи реквизита, блок", str(valueBlockNumberW + 1), ":", decodedLine, "\n"])
 
 
 			elif command == 'E9':
@@ -161,10 +166,9 @@ def process(line, command, subCommand):
 		if line[14:16] == '< ':
 		#
 			answer = line[16:18]
-
 			# Костыль по регистру 55
 			if command == '91':
-				if subCommand == '37':
+				if wholeCommandLine[19:21] == '37':
 					if line[25:27] == 'EC':
 						try:
 							error_55 = line[28:30],
@@ -176,11 +180,18 @@ def process(line, command, subCommand):
 									cursor.execute("SELECT DESC FROM REGISTER_55_FW_ERR WHERE ERR = ?", fw_err)
 									line = " ".join([line.rstrip(), cursor.fetchone()[0], "\n"])
 								except:
-									line = " ".join([line.rstrip(), "Не удалось определить: передайте автору программы, что нужно исправить чтение бинарной маски ===+", "\n"])
+									line = " ".join([line.rstrip(), bitMaskError, "\n"])
 						except:
 							line = " ".join([line.rstrip(), "Неизвестная ошибка обновления (проверьте обновление программы) ===+ \n"])
 
 
+			elif command == "E9":
+				if int(wholeCommandLine[25:27], 16) == 0:  # Здесь проверяется номер блока.
+					printString = line[37:].rstrip()
+				else:
+					printString = line[25:].rstrip()
+				decode(printString, "outer")		
+				line = "".join([line.rstrip(), " \"", decodedLine, "\" \n"])
 
 			# <55h> <Код ошибки (2)>
 			elif answer == '55' and len(line) == 26:  # <-- только для ответов длиной 26 !
@@ -195,22 +206,35 @@ def process(line, command, subCommand):
 					var = line[19:21],
 					cursor.execute("SELECT DESC FROM STATUS_CODE WHERE BIN = ?", var)
 					line = " ".join([line.rstrip(), cursor.fetchone()[0], "\n"])
+				#  Расшифровка печати (не требуется, потому что в логах и так отображается в явном виде).
+				# if command == "87": # Конкретно к какой комманде применим
+					# printString = printString[52:].rstrip()
+					# decode(printString, "outer")
+					# line = "".join([line.rstrip(), ", текст: ", decodedLine, " \n"])
+					
 		
-						
-	
-	
-
-		# ЗАКОНЧИЛИСЬ ОБРАБОТЧИКИ СТРОК
 		
 	return line
-	
-
-#
-# FIN
-#
 
 
-#  ... Удалить устаревший файл
+#  Раскодировка текста.
+def decode(printString, type):
+	global decodedLine
+	decodedLine = ''
+	if type == 'inner':
+		for char in range(0, len(printString), 3):
+			cp_hex = printString[char:char+2],
+			cursor.execute("SELECT CHAR_INNER FROM CODEPAGE WHERE HEX = ?", cp_hex)
+			decodedLine = "".join([decodedLine, cursor.fetchone()[0]])
+	else:
+		for char in range(0, len(printString), 3):
+			cp_hex = printString[char:char+2],
+			cursor.execute("SELECT CHAR_OUTER FROM CODEPAGE WHERE HEX = ?", cp_hex)
+			decodedLine = "".join([decodedLine, cursor.fetchone()[0]])
+	return decodedLine
+
+
+#  Удаление устаревшего файла со словарем.
 os.chdir(os.path.dirname(__file__))
 try:
 	os.remove('dict.py')
